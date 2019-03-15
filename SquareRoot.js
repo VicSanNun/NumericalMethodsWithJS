d560ddad00d6e7e935da3d02c336d42368a83097
Victor Nunes <victornunes470@gmail.com>
2019-03-15 00:15:20 -0300

    Refatorei de 'var' para 'let' e ao invés de utilizar console.log, para mostrar o resultado final, coloquei um return

--- SquareRoot.js
+++ SquareRoot.js
@@ -1,7 +1,7 @@
 function SquareRoot(number, step){
-  var x = 1
-  for(i=0; i < step; i++){
+  let x = 1
+  for(let i = 0; i < step; i++){
     x = 0.5*(x+(number/x))
   }
-  console.log(x)
+  return(x)
 }
